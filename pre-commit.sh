4c13204f4d6db5835d6c4cd013d164d5e1c9a3a3
alex* <31458482+alexasterisk@users.noreply.github.com>
2023-02-08 12:48:17 -0500

    use pre-commit pt3

--- pre-commit.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-printf "***** Running pre-commit hook *****\n"
-git stash -q --keep-index
-./gradlew test
-status=$?
-git stash pop -q
-printf "***** Pre-commit hook complete *****\n"
-exit $status
